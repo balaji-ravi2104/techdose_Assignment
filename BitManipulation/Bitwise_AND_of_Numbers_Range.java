bc4f2e9584d19c4678dfa1e599748a6fdd29cf39
balaji-ravi2104 <108446931+balaji-ravi2104@users.noreply.github.com>
2022-09-08 09:49:07 +0530

    Add files via upload

---
+++ BitManipulation/Bitwise_AND_of_Numbers_Range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int a=0;
+        while(left!=right){
+            left>>=1;
+            right>>=1;
+            a++;
+        }
+        return left<<a;
+    }
+}
