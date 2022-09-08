2ef9e5ea8670b3e7a2318f1aa9b0b5be03ca1757
balaji-ravi2104 <108446931+balaji-ravi2104@users.noreply.github.com>
2022-09-08 11:11:26 +0530

    Update Compliment.c

--- BitManipulation/Compliment.c
+++ BitManipulation/Compliment.c
@@ -3,13 +3,13 @@ int main(){
   int n;
   scanf("%d",&n);
   // once compliment just flip all
-  printf("%d",~n);
+  printf("%d\n",~n);
   // tow's compliment
-  printf("%d",(~n)+1);
+  printf("%d\n",(~n)+1);
   // once compliment of MSB
   for(int i=0;(n>>i)!=0;i++){
     n ^= 1<<i;
   }
-  printf("%d",n);
+  printf("%d\n",n);
   return 0;
 }
