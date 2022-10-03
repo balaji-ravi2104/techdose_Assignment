4bd1513558a8246175c7b7476c8f0d474e37a395
balaji-ravi2104 <108446931+balaji-ravi2104@users.noreply.github.com>
2022-10-03 10:26:37 +0530

    Create 4SumII.java

---
+++ Array/4SumII.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int count=0;
+        for(int i:nums1)
+            for(int j:nums2)
+                map.put(i+j,map.getOrDefault(i+j,0)+1);
+        for(int i:nums3)
+            for(int j:nums4)
+                count+=map.getOrDefault(-(i+j),0);
+        return count;
+    }
+}
+/*
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int count=0;
+        for(int i:nums1)
+            for(int j:nums2)
+                map.put(i+j,map.getOrDefault(i+j,0)+1);
+        for(int i:nums3)
+            for(int j:nums4)
+                count+=map.getOrDefault(-(i+j),0);
+        return count;
+    }
+}*/
