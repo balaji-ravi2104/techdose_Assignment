cecc834386f39959750642be474ec4ccde8067cf
balaji-ravi2104 <108446931+balaji-ravi2104@users.noreply.github.com>
2023-01-01 18:48:08 +0530

    Update Unique Paths II.java

--- DP/Unique Paths II.java
+++ DP/Unique Paths II.java
@@ -34,6 +34,21 @@ class Solution {
     }
 }
 /*
+space optimized solution
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int width = obstacleGrid[0].length;
+    int[] dp = new int[width];
+    dp[0] = 1;
+    for (int[] row : obstacleGrid) {
+        for (int j = 0; j < width; j++) {
+            if (row[j] == 1)
+                dp[j] = 0;
+            else if (j > 0)
+                dp[j] += dp[j - 1];
+        }
+    }
+    return dp[width - 1];
+    }
 You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). 
 The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 
